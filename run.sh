1debc9a34a846e27de56e8c0f55034b3fed69f6e
lipey1 <ttv.lipey@gmail.com>
2024-12-25 00:36:16 -0300

    1.1.4

--- run.sh
+++ run.sh
@@ -193,6 +193,54 @@ install_ssh() {
         ;;
     esac
 
+    # Configurar grupos e usuários
+    log "INFO" "Configuring users and groups..." "$YELLOW"
+    
+    # Criar grupos necessários
+    cat >> /etc/group <<EOL
+root:x:0:
+daemon:x:1:
+bin:x:2:
+sys:x:3:
+adm:x:4:
+tty:x:5:
+disk:x:6:
+lp:x:7:
+mail:x:8:
+news:x:9:
+uucp:x:10:
+man:x:12:
+proxy:x:13:
+kmem:x:15:
+dialout:x:20:
+fax:x:21:
+voice:x:22:
+cdrom:x:24:
+floppy:x:25:
+tape:x:26:
+sudo:x:27:
+audio:x:29:
+dip:x:30:
+www-data:x:33:
+backup:x:34:
+operator:x:37:
+list:x:38:
+irc:x:39:
+src:x:40:
+gnats:x:41:
+shadow:x:42:
+utmp:x:43:
+video:x:44:
+sasl:x:45:
+plugdev:x:46:
+staff:x:50:
+games:x:60:
+users:x:100:
+nogroup:x:65534:
+ssh:x:988:
+container:x:999:
+EOL
+
     # Configurar senha root
     echo "root:vps123" | chpasswd
 
@@ -234,18 +282,10 @@ install_ssh() {
     log "INFO" "Username: root" "$GREEN"
     log "INFO" "Password: vps123" "$GREEN"
 
-    # Criar script de autorun
-    cat > /autorun.sh <<EOL
-#!/bin/bash
-
-# Iniciar SSH se instalado
-if [ -f "/etc/dropbear/dropbear_rsa_host_key" ]; then
-    dropbear -E -F -p ${SSH_PORT:-22} &
-fi
-EOL
-
-    # Tornar o script executável
-    chmod +x /autorun.sh
+    # Atualizar /etc/passwd se necessário
+    if ! grep -q "^container:" /etc/passwd; then
+        echo "container:x:999:999:Container User:/home/container:/bin/bash" >> /etc/passwd
+    fi
 }
 
 # Function to print a beautiful help message
